a4a67c59c24c9d03fd951c7a3ce3485878ec7f6e
Jemo <jamesshisia6@gmail.com>
2023-08-12 16:51:19 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module chat.gpt.chatgpt_desktop {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens chat.gpt.chatgpt_desktop to javafx.fxml;
+    exports chat.gpt.chatgpt_desktop;
+}
